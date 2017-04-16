c45bcbac0d08fc8deaf8cc28777dd84b16d77dee
GaneshPg <pgshriganesh@gmail.com>
2017-04-16 19:05:25 +0530

    Add aim square

--- Source.cpp
+++ Source.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-float scale = 1,scalefactor = 0.2;    //scalefactor to generate different speeds for different asteroids
+float scale = 1, scalefactor = 0.2;    //scalefactor to generate different speeds for different asteroids
 float cx = 0, cy = 0, r = 1, z = 10;
 
 //TODO : Random number of asteroids, random timing, shooting controls, aim etc. (done: Try different speeds for different asteroids)
@@ -21,15 +21,15 @@ void idle() {
 void display() {
 	glClearColor(0, 0, 0, 1);
 	glClear(GL_COLOR_BUFFER_BIT);
-	
+
 	//Space background
-	float xpos,ypos,numStars=50;
-	while (numStars--){
+	float xpos, ypos, numStars = 50;
+	while (numStars--) {
 		xpos = (rand() % 20) - 10;
 		ypos = (rand() % 20) - 10;
-		glColor3f(1,1,1);
+		glColor3f(1, 1, 1);
 		glBegin(GL_POINTS);
-			glVertex3f(xpos,ypos,-10);
+		glVertex3f(xpos, ypos, -10);
 		glEnd();
 	}
 
@@ -39,50 +39,60 @@ void display() {
 	for (float i = 0; i < 180; i += 0.01) {
 		float x1 = cx + scale*r*cos(i*3.14);
 		float y1 = cy + scale*r*sin(i*3.14);
-		float x2 = cx + scale*r*cos((360-i)*3.14);
-		float y2 = cy + scale*r*sin((360-i)*3.14);
+		float x2 = cx + scale*r*cos((360 - i)*3.14);
+		float y2 = cy + scale*r*sin((360 - i)*3.14);
 		glBegin(GL_LINES);
 		glColor3f(1, 1, 1);
-		glVertex3f(x1, y1, z );
-		glVertex3f(x2, y2, z );
+		glVertex3f(x1, y1, z);
+		glVertex3f(x2, y2, z);
 		glColor3f(1, 0, 0);
-		glVertex3f(x1, y1, z );
-		glVertex3f(x2, y2, z );
+		glVertex3f(x1, y1, z);
+		glVertex3f(x2, y2, z);
 		glEnd();
 	}
 	if (scale > 5) {
 		r = 1;
 		z = 10;
 		scale = 1;
-		scalefactor = (rand()%5+1)/10.0;    //scalefactor to generate different speeds for different asteroids
-		//this_thread::sleep_for(chrono::milliseconds(rand()%5000)); // Figure out how to add delay without causing lag in controls
+		scalefactor = (rand() % 5 + 1) / 10.0;    //scalefactor to generate different speeds for different asteroids
+												  //this_thread::sleep_for(chrono::milliseconds(rand()%5000)); // Figure out how to add delay without causing lag in controls
 		cx = (rand() % 20) - 10;
 		cy = (rand() % 20) - 10;
 	}
 	else
 		glutIdleFunc(idle);
+
+	//Aim square
+	glBegin(GL_LINE_LOOP);
+	glColor3f(1, 1, 1);
+	glVertex3f(-1, -1, 0);
+	glVertex3f(1, -1, 0);
+	glVertex3f(1, 1, 0);
+	glVertex3f(-1, 1, 0);
+	glEnd();
+	
 	glFlush();
 }
 
 //Controls: functionality for normal keys like a,w,s,d
 void keyboard(unsigned char ch, int x, int y) {
 	//Figure out how to avoid the lag with key press and hold
-	 switch(ch) {
-		 case 'a': cx += 1; break;
-		 case 'd': cx -= 1; break;
-		 case 'w': cy -= 1; break;
-		 case 's': cy += 1; break;
+	switch (ch) {
+	case 'a': cx += 1; break;
+	case 'd': cx -= 1; break;
+	case 'w': cy -= 1; break;
+	case 's': cy += 1; break;
 	}
-	 glutPostRedisplay();
+	glutPostRedisplay();
 }
 
 //Controls: functionality for special function keys(direction keys) like up, down, left, right arrows
-void arrowKeyPress(int key, int x, int y){
-	switch(key){
-		case GLUT_KEY_LEFT: cx += 1; break;
-		case GLUT_KEY_RIGHT: cx -= 1; break;
-		case GLUT_KEY_UP: cy -= 1; break;
-		case GLUT_KEY_DOWN: cy += 1; break;
+void arrowKeyPress(int key, int x, int y) {
+	switch (key) {
+	case GLUT_KEY_LEFT: cx += 1; break;
+	case GLUT_KEY_RIGHT: cx -= 1; break;
+	case GLUT_KEY_UP: cy -= 1; break;
+	case GLUT_KEY_DOWN: cy += 1; break;
 	}
 	glutPostRedisplay();
 }
