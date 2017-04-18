129a7a6675eb01a85e955fb95080d9e555402536
Sweta Kumari <sweta191995@gmail.com>
2017-04-18 18:31:46 +0530

    Added score, modified laser and aim

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,7 @@
+#include <iostream>
 #include <cmath>
 #include <chrono>
+#include <ctime>
 #include <thread>
 #include <GL/glut.h>
 
@@ -9,8 +11,14 @@ float scale = 1, scalefactor = 0.2;    //scalefactor to generate different speed
 float cx = 0, cy = 0, r = 1, z = -10;
 int flag = 0;
 
-//TODO : Random number of asteroids, random timing, shooting controls, aim etc. (done: Try different speeds for different asteroids)
-// Still remain smooth
+//TODO: Random number of asteroids at the same time, random timing,  etc. 
+//TODO: gameplay should be smooth
+//TODO: game over, opening screen, menus, asteroid texture, panel at the bottom of the screen, healthbar?, enter to pause the game
+//TODO: work on laser, prevent continuous key press
+
+//done: Try different speeds for different asteroids, shooting controls, aim
+//aim still takes a square region for hit(make it circle), add special scores for hitting the asteroids
+
 
 /*void idle() {
 	this_thread::sleep_for(chrono::milliseconds(50));
@@ -20,6 +28,19 @@ int flag = 0;
 }*/
 
 int delay = 100;
+
+unsigned long long int score = 0;
+
+time_t clk;
+
+void idle(){
+	time_t now = time(NULL);
+	if(difftime(now,clk) > 5){
+		score += 10;
+		clk = now;
+		cout<<score<<endl;
+	}
+}
 
 void timer(int val){
 	if(scale < 4)
@@ -61,10 +82,11 @@ void display() {
 		glVertex3f(x2, y2, z);
 		glEnd();
 	}
+	
 	if (z <= -10) {
 		if(cx < 10 && cx > -10 && cy < 10 && cy > -10 && flag){
-			glClear(GL_COLOR_BUFFER_BIT);
 			glClearColor(1,1,1,1);
+			glClear(GL_COLOR_BUFFER_BIT);
 			
 		}
 		else{
@@ -82,13 +104,16 @@ void display() {
 		}
 	}
 	
-	//Aim square
-	glBegin(GL_LINE_LOOP);
-	glColor3f(1, 1, 1);
-	glVertex3f(-1, -1, 0);
-	glVertex3f(1, -1, 0);
-	glVertex3f(1, 1, 0);
-	glVertex3f(-1, 1, 0);
+	//Drawing the pointer for aim
+	glBegin(GL_LINES);
+		glColor3f(1, 1, 1);
+		glVertex3f(-1, 0, 0);
+		glVertex3f(1, 0, 0);
+	glEnd();
+	glBegin(GL_LINES);
+		glColor3f(1, 1, 1);
+		glVertex3f(0, 1, 0);
+		glVertex3f(0, -1, 0);
 	glEnd();
 	
 	glFlush();
@@ -103,22 +128,40 @@ void keyboard(unsigned char ch, int x, int y) {
 	case 'w': cy -= 1; break;
 	case 's': cy += 1; break;
 	case 32:
-	glLineWidth(10);    //doesn't work
-	glBegin(GL_LINES);
-	glColor3f(0,1,0);
-	glVertex3f(-10,-10,0);
-	glVertex3f(0,0,0);
-	glVertex3f(10,-10,0);
-	glVertex3f(0,0,0);
-	glEnd();
-	glFlush();
-	glLineWidth(1);
+		//laser shoots to the center of the screen
+		glPointSize(5);   
+		glBegin(GL_LINES);
+			glColor3f(0,1,0);
+			glVertex3f(-10,-10,0);
+			glVertex3f(-8,-8,0);
+			glVertex3f(-7,-7,0);
+			glVertex3f(-5,-5,0);
+			glVertex3f(-4,-4,0);
+			glVertex3f(-2,-2,0);
+			glVertex3f(-1,-1,0);
+			glVertex3f(0,0,0);
+			
+			glVertex3f(10,-10,0);
+			glVertex3f(8,-8,0);
+			glVertex3f(7,-7,0);
+			glVertex3f(5,-5,0);
+			glVertex3f(4,-4,0);
+			glVertex3f(2,-2,0);
+			glVertex3f(1,-1,0);
+			glVertex3f(0,0,0);
+		glEnd();
+		glFlush();
+		//glLineWidth(1);
 	
-	if(0 < (cx + (scale *r)) && 0 > (cx - (scale*r)) && 0 < (cy + (scale *r)) && 0 > (cy - (scale*r)) ){
-		cx = 1000;
-		cy = 1000;
-	
-	}
+		//when the laser hits the asteroid
+		//double theta = atan(cy/cx);
+		//if(0 < (cx + (scale *r)) && 0 > (cx - (scale*r)) && 0 < (cy + (scale *r)) && 0 > (cy - (scale*r)) ){
+		if(((cx*cx) + (cy*cy)) < (scale*scale*r*r)){
+			cx = 1000;
+			cy = 1000;
+			//giving special score if you hit the asteroid
+			score += 50;
+		}
 	}
 	glutPostRedisplay();
 }
@@ -147,8 +190,10 @@ int main(int argc, char *argv[]) {
 	glutInitWindowSize(800, 800); //Choose a good window size. Maybe use screen size - Full screen stuff?
 	glutCreateWindow("Asteroids!");
 	init();
+	clk = time(NULL);
 	glutDisplayFunc(display);
 	glutKeyboardFunc(keyboard);
+	glutIdleFunc(idle);
 	glutSpecialFunc(arrowKeyPress);
 	glutMainLoop();
 }
